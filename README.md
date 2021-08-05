1192209c19c99a28dabfe72dc9de8c24239a2c6e
Tomáš Konrády <tomaskonrady@gmail.com>
2021-08-05 12:38:22 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,87 @@
+<h1 align="center">
+  <a href="https://github.com/lundegaard/eslint-config-lundegaard">eslint-config-lundegaard</a>
+</h1>
+
+<p align="center">
+  <a href="https://lundegaard.eu">
+    <img alt="by Lundegaard" src="https://github.com/lundegaard/react-union/raw/master/by-lundegaard.png" width="120" />
+  </a>
+</p>
+
+<h3 align="center">
+🖍️ 🛡  🚀
+</h3>
+
+<h3 align="center">
+Have a good start with your next JavaScript project.
+</h3>
+
+<p align="center">
+The package provides `.eslintrc.*` as an extensible shared config.
+</p>
+
+<p align="center">
+
+
+<a href="https://github.com/lundegaard/eslint-config-lundegaard">
+<img src="https://flat.badgen.net/badge/-/github?icon=github&label" alt="Github" />
+</a>
+
+<img src="https://flat.badgen.net/badge/license/MIT/blue" alt="MIT License" />
+
+<a href="https://www.npmjs.com/package/eslint-config-lundegaard">
+<img src="https://flat.badgen.net/npm/dm/eslint-config-lundegaard" alt="Downloads" />
+</a>
+
+<a href="https://www.npmjs.com/package/eslint-config-lundegaard">
+<img src=" https://flat.badgen.net/npm/v/eslint-config-lundegaard" alt="Version" />
+</a>
+</p>
+
+#
+
+The package provides `.eslintrc.*` as an extensible shared configuration.
+
+## Usage
+
+There are two ESLint configurations for you to use.
+
+### eslint-config-lundegaard
+
+The default export contains all of our ESLint rules, including ECMAScript 6+ and React. It requires `eslint`, `babel-eslint`, `eslint-plugin-babel`, `eslint-plugin-import` and `eslint-plugin-react`.
+
+### Installation
+
+```sh
+yarn add -D eslint-config-lundegaard @babel/eslint-parser@7.15.0 @babel/eslint-plugin@7.14.5 eslint@7.32.0 eslint-plugin-import@2.23.4 eslint-plugin-react@7.24.0
+```
+
+or
+```sh
+npm install eslint-config-lundegaard @babel/eslint-parser@7.15.0 @babel/eslint-plugin@7.14.5 eslint@7.32.0 eslint-plugin-import@2.23.4 eslint-plugin-react@7.24.0 --save-dev
+```
+
+And add `"extends": "lundegaard"` to your .eslintrc.
+
+### eslint-config-lundegaard/base
+
+The eslint-config-lundegaard without excluding rules for React.
+
+Add `"extends": "eslint-config-lundegaard/base"` to your `.eslintrc.*`.
+
+## Related projects
+
+- [react-union](https://github.com/lundegaard/react-union) – Integrate React apps into various CMSs seamlessly.
+- [validarium](https://github.com/lundegaard/validarium) – Platform-agnostic validation library for JavaScript.
+- [lundium](https://github.com/lundegaard/lundium) – Beautiful React component library.
+- [@redux-tools](https://github.com/lundegaard/redux-tools) – Maintaining large Redux applications with ease.
+
+## Contributing
+
+We are open to all ideas and suggestions, feel free to open an issue or a pull request!
+
+See the [contribution guide](https://github.com/lundegaard/eslint-config-lundegaard/blob/master/CONTRIBUTING.md) for guidelines.
+
+## License
+
+All packages are distributed under the MIT license. See the license [here](https://github.com/lundegaard/eslint-config-lundegaard/blob/master/LICENSE).
---
+++ rules/base.js
@@ -0,0 +1,134 @@
+module.exports = {
+	parser: '@babel/eslint-parser',
+	parserOptions: {
+		ecmaVersion: 2018,
+		sourceType: 'module',
+	},
+	env: {
+		browser: true,
+		commonjs: true,
+		es6: true,
+		jest: true,
+		node: true,
+	},
+	plugins: ['@babel', 'import'],
+	rules: {
+		'array-bracket-spacing': ['error', 'never'],
+		'array-callback-return': 'error',
+		'arrow-body-style': ['error', 'as-needed'],
+		'arrow-parens': ['error', 'as-needed'],
+		'arrow-spacing': 'error',
+		'@babel/new-cap': 'error',
+		'block-scoped-var': 'error',
+		'block-spacing': ['error', 'always'],
+		'brace-style': 'error',
+		'comma-dangle': ['error', 'always-multiline'],
+		'comma-spacing': [
+			'error',
+			{
+				before: false,
+				after: true,
+			},
+		],
+		'comma-style': ['error', 'last'],
+		'computed-property-spacing': ['error', 'never'],
+		'consistent-return': 'off',
+		'consistent-this': ['error', 'self'],
+		curly: 'error',
+		'dot-location': ['error', 'property'],
+		'dot-notation': 'error',
+		'eol-last': 'error',
+		eqeqeq: ['error', 'smart'],
+		'generator-star-spacing': 'error',
+		'id-blacklist': ['error'],
+		'key-spacing': 'error',
+		'keyword-spacing': 'error',
+		'max-len': [
+			'error',
+			{
+				// "Lines of > 80 characters are really the last jabs
+				// at the dying corpses of typography and taste in the digital world.
+				//
+				// Typographically proper paragraphs are not wide across,
+				// because that forces the eye to move more while reading.
+				//
+				// This is a basic fact that has been known for literally longer
+				// than we've had printing presses."
+				//
+				// See:
+				// https://katafrakt.me/2017/09/16/80-characters-line-length-limit/#properties-of-the-human-eyes
+				// https://nickjanetakis.com/blog/80-characters-per-line-is-a-standard-worth-sticking-to-even-today#reading-code
+				// https://hackernoon.com/does-column-width-of-80-make-sense-in-2018-50c161fbdcf6
+				//
+				// TLDR:
+				// - better for your eyes
+				// - quicker undertanding of the code
+				// - possibility of multiple files side by side on the same display
+				code: 80,
+				tabWidth: 2,
+				ignoreComments: false,
+				ignoreStrings: true,
+				ignoreRegExpLiterals: true,
+				ignoreTemplateLiterals: true,
+				ignoreUrls: true,
+			},
+		],
+		'new-cap': [
+			'off',
+			{
+				capIsNew: true,
+				newIsCap: true,
+			},
+		],
+		'no-array-constructor': 'error',
+		'no-await-in-loop': 'error',
+		'no-case-declarations': 'error',
+		'no-cond-assign': 'error',
+		'no-console': 'error',
+		'no-const-assign': 'error',
+		'no-dupe-args': 'error',
+		'no-dupe-keys': 'error',
+		'no-duplicate-case': 'error',
+		'no-empty-pattern': 'error',
+		'no-extra-boolean-cast': 'error',
+		'no-extra-semi': 'error',
+		'no-loop-func': 'error',
+		'no-multi-spaces': 'error',
+		'no-multiple-empty-lines': 'error',
+		'no-shadow': 'off',
+		'no-spaced-func': 'error',
+		'no-trailing-spaces': 'error',
+		'no-undef': 'error',
+		'no-unneeded-ternary': 'error',
+		'no-unreachable': 'error',
+		'no-unused-expressions': 'error',
+		'no-unused-vars': 'error',
+		'no-var': 'error',
+		'object-shorthand': 'error',
+		'one-var': ['error', 'never'],
+		'operator-linebreak': 'off',
+		'padded-blocks': ['error', 'never'],
+		'prefer-arrow-callback': 'off',
+		'prefer-const': 'error',
+		'prefer-spread': 'error',
+		'prefer-template': 'error',
+		quotes: ['error', 'single', 'avoid-escape'],
+		'space-before-blocks': ['error', 'always'],
+		semi: ['error', 'always'],
+		'space-before-function-paren': [
+			'error',
+			{ anonymous: 'never', named: 'never', asyncArrow: 'always' },
+		],
+		'space-infix-ops': 'error',
+		'space-unary-ops': [
+			'error',
+			{
+				words: true,
+				nonwords: false,
+			},
+		],
+		'spaced-comment': 'error',
+		strict: 'off',
+		yoda: 'error',
+	},
+};
